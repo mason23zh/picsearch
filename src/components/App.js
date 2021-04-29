54c921ad3d5739001121f9930dc45a6cf17bdc61
Zhengyang Zhao <mason.zh@gmail.com>
2021-04-28 19:41:42 -0500

    add Axios & Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+import UnsplashAuth from "../unsplashAuth";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    //HTTP Request with Axios
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: UnsplashAuth.getAuthClientID(),
+      },
+    });
+
+    console.log(res);
   }
 
   render() {
